5bbdbd7596696fa4cb5ebd48d021bd75dd6101a3
Israel Herrera <israel@construex.com.mx>
2024-09-11 22:14:52 -0500

    chore: Update mini-session page to pass slug prop to ContentSteps component

--- src/app/mini-session/page.tsx
+++ src/app/mini-session/page.tsx
@@ -1,10 +1,18 @@
+import { FC } from "react";
 import ContentSteps from "@/components/content-steps";
 import TotalPrice from "@/components/total-price";
 
-const MiniSessionPage = () => {
+interface SessionPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const MiniSessionPage: FC<SessionPageProps> = ({ params }) => {
+  const { slug } = params;
   return (
     <section className="flex flex-col-reverse lg:flex-row justify-start items-start gap-8 py-8 container">
-      <ContentSteps />
+      <ContentSteps slug={slug} />
       <TotalPrice slug={"mini-session"} />
     </section>
   );
